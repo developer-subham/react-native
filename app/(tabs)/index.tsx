0fc82e2222d2e0b8a1609987d913dbe82d16f53d
Subham Behera <subhambehera2104@gmail.com>
2026-03-22 05:34:00 +0530

    Added css in index page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,30 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function Home() {
   return (
-    <View>
-      <Text>Hello World 🚀</Text>
+    <View style={{
+      flex: 1,
+      backgroundColor: '#0c1420',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      
+      <Text style={{
+        color: 'white',
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginBottom: 10
+      }}>
+        Hello Subham 🚀
+      </Text>
+
+      <Text style={{
+        color: 'gray',
+        fontSize: 18
+      }}>
+        Learning React Native 🔥
+      </Text>
+
     </View>
   );
-};
+}
